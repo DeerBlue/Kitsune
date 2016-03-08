aa73f730a7158e1e5c87504a350b9b98dcdc0e66
Deer Blue <puffleskate@gmail.com>
2016-03-08 10:27:33 -0500

    Update Friends.php

--- Kitsune/ClubPenguin/Handlers/Play/Friends.php
+++ Kitsune/ClubPenguin/Handlers/Play/Friends.php
@@ -1,2 +1,10 @@
 <?php
+namespace Kitsune\ClubPenguin\Handlers\Play;
+use Kitsune\Logging\Logger;
+use Kitsune\ClubPenguin\Packets\Packet;
+trait Friends {
+  public function addBuddy ($socket){
+    $penguin = $this->penguins[$socket];
+  }
+}
 ?>
